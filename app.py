2eb35bbb01417866407d3374a7b0a1904ebdfc08
Jes1326 <jesshu39@gmail.com>
2025-03-03 09:04:00 +0530

    Final

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 print("Hello World")
 print("Adding features")
 
-<<<<<<< HEAD
 print("Adding feature 3")
 
 print("Adding feature 4")
-=======
 print("Adding feature 1")
 
 print("Adding feature 2")
->>>>>>> features
